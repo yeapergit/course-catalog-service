930f6efd8583fe651a475a25d840c5d8fdf61ece
Pedro Ferreira <yeaper@gmail.com>
2024-01-23 17:12:26 +0000

    add database config, DTO and entity model

---
+++ src/main/kotlin/com/kotlinspring/entity/Course.kt
@@ -0,0 +1,13 @@
+package com.kotlinspring.entity
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Courses")
+data class Course(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Int?,
+    val name: String,
+    val category: String
+)
---
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -0,0 +1,8 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.entity.Course
+import org.springframework.data.repository.CrudRepository
+
+interface CourseRepository : CrudRepository<Course, Int> {
+
+}
