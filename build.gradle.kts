3fa91032dc8f18ed7974f1f24b42d8da0e60d180
Pedro Ferreira <yeaper@gmail.com>
2024-01-23 15:15:05 +0000

    Create GreetingContoller and service, add configuration files and logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	//logging
+	implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+
 	runtimeOnly("com.h2database:h2")
 	//runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/com/kotlinspring/controller/GreetingController.kt
@@ -0,0 +1,21 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.service.GreetingsService
+import mu.KLogging
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/greetings")
+class GreetingController(val greetingsService: GreetingsService) {
+
+    companion object : KLogging()
+
+    @GetMapping("/{name}")
+    fun retrieveGreeting(@PathVariable("name") name: String): String {
+        logger.info("Name is $name")
+        return greetingsService.retrieveGreeting(name);
+    }
+}
